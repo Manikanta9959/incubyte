b7297a5c14eaabd6eaae56b3ad3069a01a8cf0c1
Manikanta9959 <sivachallano1@gmail.com>
2025-10-01 23:16:29 +0530

    removed default init function modified code

--- calculator.py
+++ calculator.py
@@ -1,53 +1,71 @@
 import re
+from typing import List, Tuple
 
-class StringCalc():
+
+class StringCalculator:
     
-    def __init__(self, str_num : str):
-        self.str_num = str_num
-        
-    
-    def add(self):
-        if not self.str_num:
+    def add(self, numbers: str) -> int:
+        if not numbers:
             return 0
         
-        delimiter = ","
-        numbers_to_parse = self.str_num
+        delimiter, numbers_to_parse = self._extract_delimiter_and_numbers(numbers)
+        number_list = self._parse_numbers(numbers_to_parse, delimiter)
+        self._validate_negatives(number_list)
         
-        if self.str_num.startswith("//"):
-            delimiter_end = self.str_num.index("\n")
-            delimiter = self.str_num[2:delimiter_end]
-            numbers_to_parse = self.str_num[delimiter_end + 1:]
-            
+        return sum(number_list)
+    
+    def _extract_delimiter_and_numbers(self, numbers: str) -> Tuple[str, str]:
+        if numbers.startswith("//"):
+            delimiter_end = numbers.index("\n")
+            delimiter = numbers[2:delimiter_end]
+            numbers_to_parse = numbers[delimiter_end + 1:]
+            return delimiter, numbers_to_parse
         
-        numbers_to_parse = numbers_to_parse.replace("\n", delimiter)
-        
+        return ",", numbers
+    
+    def _parse_numbers(self, numbers_str: str, delimiter: str) -> List[int]:
+        normalized_string = self._normalize_delimiters(numbers_str, delimiter)
+        number_strings = self._split_by_delimiter(normalized_string, delimiter)
+        return self._convert_to_integers(number_strings)
+    
+    def _normalize_delimiters(self, numbers_str: str, delimiter: str) -> str:
+        return numbers_str.replace("\n", delimiter)
+    
+    def _split_by_delimiter(self, numbers_str: str, delimiter: str) -> List[str]:
         escaped_delimiter = re.escape(delimiter)
-        
-        number_strings = re.split(escaped_delimiter, numbers_to_parse)
-        
-        print(number_strings)
-        
-        parsed_numbers = []
-        negative_numbers = []
-        
-        for num_str in number_strings:
-            if num_str:
-                num = int(num_str)
-                parsed_numbers.append(num)
-                if num < 0:
-                    negative_numbers.append(num)
-        
-        if negative_numbers:
-            negative_str = ", ".join(map(str, negative_numbers))
-            raise ValueError(f"negative numbers not allowed: {negative_str}")
-        
-        return sum(parsed_numbers)
+        return re.split(escaped_delimiter, numbers_str)
     
+    def _convert_to_integers(self, number_strings: List[str]) -> List[int]:
+        return [int(num_str) for num_str in number_strings if num_str]
+    
+    def _validate_negatives(self, numbers: List[int]) -> None:
+        negatives = [num for num in numbers if num < 0]
+        
+        if negatives:
+            self._raise_negative_exception(negatives)
+    
+    def _raise_negative_exception(self, negatives: List[int]) -> None:
+        negative_str = ", ".join(map(str, negatives))
+        raise ValueError(f"negative numbers not allowed: {negative_str}")
 
-str_cal_obj = StringCalc("//;\n1;2")
-print(str_cal_obj.add())
 
-        
+if __name__ == "__main__":
+    calc = StringCalculator()
     
+    print(calc.add(""))
+    print(calc.add("1"))
+    print(calc.add("1,5"))
+    print(calc.add("1,2,3,4,5"))
+    print(calc.add("1\n2,3"))
+    print(calc.add("//;\n1;2"))
+    print(calc.add("//|\n1|2|3"))
     
+    try:
+        calc.add("1,-2,3")
+    except ValueError as e:
+        print(e)
     
+    try:
+        calc.add("1,-2,-3,4,-5")
+    except ValueError as e:
+        print(e)
