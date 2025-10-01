269b0a897287fca7f855bfdb76b9f5535aa223ae
Manikanta9959 <sivachallano1@gmail.com>
2025-10-01 22:37:51 +0530

    empty string

---
+++ calculator.py
@@ -0,0 +1,25 @@
+class StringCalc():
+    
+    def __init__(self, str_num : str):
+        self.str_num = str_num
+        
+    
+    def add(self):
+        if not self.str_num:
+            return 0
+        
+    
+    
+    
+
+
+
+
+
+str_cal_obj = StringCalc("")
+print(str_cal_obj.add())
+
+        
+    
+    
+    
