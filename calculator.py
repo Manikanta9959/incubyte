50dcca5c4ddca4063d3b623e009cb4d76e1ad4c3
Manikanta9959 <sivachallano1@gmail.com>
2025-10-01 23:02:18 +0530

    new line string

--- calculator.py
+++ calculator.py
@@ -1,3 +1,5 @@
+import re
+
 class StringCalc():
     
     def __init__(self, str_num : str):
@@ -8,15 +10,31 @@ class StringCalc():
         if not self.str_num:
             return 0
         
+        delimiter = ","
+        numbers_to_parse = self.str_num
+        
+            
+        
+        numbers_to_parse = numbers_to_parse.replace("\n", delimiter)
+                
+        
+        number_strings = re.split(delimiter, numbers_to_parse)
+        
+        print(number_strings)
+        
+        parsed_numbers = []
+        
+        
+        for num_str in number_strings:
+            if num_str:
+                num = int(num_str)
+                parsed_numbers.append(num)
+        
+        
+        return sum(parsed_numbers)
     
-    
-    
 
-
-
-
-
-str_cal_obj = StringCalc("")
+str_cal_obj = StringCalc("1\n1,2,3")
 print(str_cal_obj.add())
 
         
